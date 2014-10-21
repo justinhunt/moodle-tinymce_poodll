2ecb7b708937b3d884c2d6926fd7975f5e48e153
Justin Hunt <poodllsupport@gmail.com>
2014-10-21 09:40:36 +0900

    version bump

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // The current plugin version (Date: YYYYMMDDXX).
-$plugin->version   = 2014050100;
+$plugin->version   = 2014102100;
 // Required Moodle version.
 $plugin->requires  = 2012112900;
 // Component Name - Moodle uses this as unique id for plugin
@@ -33,5 +33,5 @@ $plugin->component = 'tinymce_poodll';
 //beta
 $plugin->maturity  = MATURITY_STABLE;
 // Human readable version informatiomn
-$plugin->release   = '1.0.5 (Build 2014050100)';
+$plugin->release   = '1.0.6 (Build 2014102100)';
 $plugin->dependencies = array('filter_poodll' => 2014011500);
